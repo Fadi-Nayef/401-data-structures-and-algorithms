b43b47a1bfbd5d5183348fd779134d42a848b0ae
Fadi_Nayef <adraf38@hotmail.com>
2021-08-09 00:56:30 +0300

    "Blog26"

---
+++ CodeChallenges/Trees/app/src/main/java/Blogs/Blog.java
@@ -0,0 +1,29 @@
+package Blogs;
+
+public class Blog {
+
+    public static void main(String[] args) {
+        int arr[]= {8,4,23,42,16,15};
+        sortArr(arr);
+    }
+
+    public Blog() {
+    }
+
+    public static void sortArr(int[] arr){
+        int n = arr.length;
+
+        for (int i = 0; i <n-1 ; i++) {
+            int min = i;
+            for (int j = i+1; j < n; j++) {
+                if(arr[j]< arr[min]){
+                    min=j;
+                }
+            }
+            int temp = arr [min];
+            arr[min]= arr[i];
+            arr[i]= temp;
+            System.out.print(temp);
+        }
+    }
+}
---
+++ CodeChallenges/Trees/app/src/test/java/Trees/BlogTest.java
@@ -0,0 +1,15 @@
+package Trees;
+import Blogs.Blog;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BlogTest {
+@Test
+    public void sortArrTest(){
+    Blog sorted =new Blog();
+    int arr[]= {8,4,23,42,16,15};
+         sorted.sortArr(arr);
+assertEquals("48151623",sorted.sortArr(arr));
+}
+
+}
